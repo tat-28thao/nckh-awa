e379a56e1b38b08fa2d95c06e64d0795356c2011
tat-28thao <11242835@st.neu.edu.vn>
2025-12-23 22:37:12 +0700

    Update Award.cs

--- NCKH.Core/Entities/Award.cs
+++ NCKH.Core/Entities/Award.cs
@@ -1,1 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace NCKH.Core.Entities;
+
+public class Award
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public int MaDeTai { get; set; }
+
+    [Required]
+    public decimal Diem { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string TenGiai { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(50)]
+    public string MaGiai { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(50)]
+    public string CapGiai { get; set; } = string.Empty;
+
+    [MaxLength(500)]
+    public string?  GhiChu { get; set; }
+
+    public DateTime NgayTrao { get; set; } = DateTime.Now;
+
+    // Navigation
+    [ForeignKey("MaDeTai")]
+    public virtual Topic?  Topic { get; set; }
+}
